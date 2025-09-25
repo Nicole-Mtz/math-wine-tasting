bd9a54ff4e0050900e85e593bc1e6fa671b815c2
Nicole-Mtz <22nick0429@gmail.com>
2025-09-25 18:04:41 -0400

    Add files via upload

---
+++ Milestone 3 - MAT 327.R
@@ -0,0 +1,36 @@
+library(readr)
+library(tidyverse)
+library(conflicted)
+wine <- read_csv("https://raw.githubusercontent.com/Nicole-Mtz/math-wine-tasting/refs/heads/main/WineQT.csv")
+
+wine <- wine %>% 
+  rename(
+    fixed_acidity = `fixed acidity`,
+    volatile_acidity = `volatile acidity`,
+    citric_acid = `citric acid`,
+    residual_sugar = `residual sugar`,
+    free_sulfur_dioxide = `free sulfur dioxide`,
+    total_sulfur_dioxide = `total sulfur dioxide`
+  )
+
+View(wine)
+
+hist(wine$fixed_acidity, 
+     main = "Wine's Fixed Acidity", 
+     xlab = "Fixed Acidity (g/L)", 
+     ylab = "Number of Samples")
+
+hist(wine$residual_sugar, 
+     main = "Wine's Residual Sugar", 
+     xlab = "Residual Sugar (g/L)", 
+     ylab = "Number of Samples") 
+
+hist(wine$residual_sugar, 
+     main = "Wine's Alcohol Level", 
+     xlab = "Alcohol (%)", 
+     ylab = "Number of Samples")
+
+barplot(table(wine$quality),
+        main = "Wine Quality", 
+        xlab = "Quality Score (between 0 - 10)", 
+        ylab = "Number of Samples")
